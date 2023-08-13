4b59bcc43bedb49ba28da4b4ecd02b89fc2bf677
Oğuz Demirel <Oguzdmrl16@gmail.com>
2023-08-13 14:38:36 +0300

    Brand için Query&Command operasyonlarımız oluşturuldu. Mapping conigurasyonu hazırlandı, Controller tamamlandı.

---
+++ Application/Features/Brands/Commands/Delete/DeleteBrandCommand.cs
@@ -0,0 +1,30 @@
+﻿using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Brands.Commands.Delete;
+
+public class DeleteBrandCommand : IRequest<DeletedBrandResponse>
+{
+    public Guid Id { get; set; }
+    public class DeleteBrandCommandHandler : IRequestHandler<DeleteBrandCommand, DeletedBrandResponse>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+        }
+        public async Task<DeletedBrandResponse> Handle(DeleteBrandCommand request, CancellationToken cancellationToken)
+        {
+            Brand? brand = await _brandRepository.GetAsnyc(predicate: b => b.Id == request.Id, withDeleted: true, cancellationToken: cancellationToken);
+            await _brandRepository.DeleteAsync(brand);
+            DeletedBrandResponse response = _mapper.Map<DeletedBrandResponse>(brand);
+
+            return await Task.FromResult(response);
+        }
+    }
+}
