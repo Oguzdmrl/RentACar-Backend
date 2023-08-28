5c4e933ee23722bb30834f88a61d4a994d8e23f3
Oğuz Demirel <Oguzdmrl16@gmail.com>
2023-08-28 20:04:52 +0300

    CQRS Mediatr Logging Pipeline İmplementasyonu Hazırlandı. Loglama Altyapısının İmplementasyonu Yapıldı.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -9,9 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddHttpContextAccessor();
 
-//builder.Services.AddDistributedMemoryCache(); // => InMemory Cache
-builder.Services.AddStackExchangeRedisCache(opt => opt.Configuration = "localhost:6379"); // Redis Cache
+builder.Services.AddDistributedMemoryCache(); // => InMemory Cache
+//builder.Services.AddStackExchangeRedisCache(opt => opt.Configuration = "localhost:6379"); // Redis Cache
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -26,7 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 //if (app.Environment.IsProduction())
-app.ConfigureCustomExceptionMiddleware();
+//app.ConfigureCustomExceptionMiddleware();
 
 
 app.UseHttpsRedirection();
